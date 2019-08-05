70d65be9204db8b26239a0d37acb3ebc87f49341
= <=>
2019-08-05 11:16:20 -0700

    add sour, merge, count and print functionality

--- bomdotcom.py
+++ bomdotcom.py
@@ -1,32 +1,47 @@
 import sys
 import re
-
-
+import json
 
 def parse():
-    firstLine = True
+    i = 0
     bomCollection = []
     output = 0
     for line in sys.stdin:
-        if not firstLine:
-            bomCollection.append(BOM(line.rstrip()))
+        foundDupe = False
+        if i > 0:
+            bom = BOM(line.rstrip())
+            if i == 1:
+                bomCollection.append(bom)
+            else:
+                for b in bomCollection:
+                    if b.MPN == bom.MPN and b.Manufacturer == bom.Manufacturer:
+                        b.ReferenceDesignators.extend(rd for rd in bom.ReferenceDesignators if rd not in b.ReferenceDesignators)
+                        b.NumOccurrences += 1
+                        foundDupe = True
+                        break
+                    
+            if not foundDupe:
+                bomCollection.append(bom)
+            i += 1
 
         else:
-            output = line.rstrip()
-            firstLine = False
+            output = int(line.rstrip())
+            i += 1
     
-    print('Begin BOM data output')
+    bomCollection = sorted(bomCollection, key=lambda b: b.NumOccurrences, reverse=True)
+    
+    print('Begin BOM data output\n')
+    
     idx = 0
-    while idx <= int(output):
-        bom = bomCollection[idx]
-        print("MPN: " + bom.mpn)
-        print("Manufacturer: " + bom.manufacturer)
-        print("Reference Designators: " + str(bom.referenceDesignators))
-        print('\n')
+    final = []
+    while idx < output:
+        final.append(bomCollection[idx].__dict__)
         idx += 1
+    
+    print(json.dumps(final, indent=4))
 
 class BOM:
-    """ A basic class to encapsulate BOM data from a variety of formats"""
+    """ A basic class to capture BOM data from a variety of formats"""
 
     formatVersionRxDict = {
             1: re.compile(r'([^:]*:[^:]*:[^:]*)'),
@@ -35,34 +50,34 @@ class BOM:
         }
 
     def parseFormatVersion(self, originalBOM):
-
         for key, rx in self.formatVersionRxDict.items():
             match = rx.search(originalBOM)
             if match:
-                self.formatVersion = key
+                return key
 
         return None
 
-    def setBOMData(self, originalBOMString):
-        if self.formatVersion == 1:
+    def setBOMData(self, originalBOMString, formatVersion):
+        if formatVersion == 1:
             bomArray = originalBOMString.split(':')
-            self.mpn = bomArray[0]
-            self.manufacturer = bomArray[1]
-            self.referenceDesignators = bomArray[2].split(',')
-        elif self.formatVersion == 2:
+            self.MPN = bomArray[0]
+            self.Manufacturer = bomArray[1]
+            self.ReferenceDesignators = bomArray[2].split(',')
+        elif formatVersion == 2:
             bomArray = originalBOMString.split(' -- ')
-            self.manufacturer = bomArray[0]
-            self.mpn = bomArray[1].split(':')[0]
-            self.referenceDesignators = bomArray[1].split(':')[1].split(',')
-        elif self.formatVersion == 3:
+            self.MPN = bomArray[1].split(':')[0]
+            self.Manufacturer = bomArray[0]
+            self.ReferenceDesignators = bomArray[1].split(':')[1].split(',')
+        elif formatVersion == 3:
             bomArray = originalBOMString.split(';')
-            self.referenceDesignators = bomArray[0].split(',')
-            self.mpn = bomArray[1]
-            self.manufacturer = bomArray[2]
+            self.MPN = bomArray[1]
+            self.Manufacturer = bomArray[2]
+            self.ReferenceDesignators = bomArray[0].split(',')
+        
+        self.NumOccurrences = 1
 
     def __init__(self, originalBOMString):
-        self.originalBOMString = originalBOMString
-        self.parseFormatVersion(originalBOMString)
-        self.setBOMData(originalBOMString)
+        formatVersion = self.parseFormatVersion(originalBOMString)
+        self.setBOMData(originalBOMString, formatVersion)
 
 parse()
